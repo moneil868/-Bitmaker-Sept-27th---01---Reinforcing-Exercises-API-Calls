c9b929ea3890d7e9ab26073e2fd65f69452e9379
Marlon O'Neil <hello@marlononeil.com>
2017-09-29 16:13:17 -0400

    Completed 'Exercise 2'

--- exercise.rb
+++ exercise.rb
@@ -9,3 +9,19 @@ toronto_wards_json = JSON.parse(toronto_wards_response.body)
 # Exercise 1
 # Iterate through your new hash and display the name of each ward in the collection.
 toronto_wards_json["objects"].each { |object| puts object["name"]}
+
+
+
+
+
+# Let's make the first API call to this URL ("endpoint") together:
+candidates_response = HTTParty.get('https://represent.opennorth.ca/candidates/')
+
+# Now we can convert that JSON response into a Ruby hash:
+candidates_response_json = JSON.parse(candidates_response.body)
+
+puts
+puts
+# Exercise 1
+# Iterate through your new hash and display the name of each ward in the collection.
+candidates_response_json["objects"].each { |object| puts "#{object["district_name"]}: #{object["name"]}"}
